b2cfbb59ecfd7141c99821d04fb221976144f25f
Jingyu Xie <96410222+ValerieTse@users.noreply.github.com>
2025-04-14 14:45:37 -0700

    Create prepare_pop.r

---
+++ prepare_pop.r
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+# Load required packages
+suppressMessages(library(dplyr))
+
+# Get command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) != 2) {
+  stop("Usage: Rscript prepare_pop.r <fam_file_path> <pop_file> <output_prefix>")
+}
+
+fam_file <- args[1]
+pop_file <- args[2]
+prefix <- args[3]
+
+df <- read.table(pop_file, header = TRUE, sep = ",")
+df$SPop <- ifelse(df$SPop == "Sample","-",df$SPop)
+
+df2 <- read.table(fam_file)
+colnames(df2) <- c("FID","IID","V3","V4","V5","V6")
+
+df <- merge(df2, df, by="IID", all.x = TRUE)
+df <- df %>% select(SPop) 
+
+write.table(df, paste0("DATA/", prefix, ".pop"), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
