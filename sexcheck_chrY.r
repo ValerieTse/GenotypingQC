8067776a571368cca0efbd86acd368cb77a746e0
Jingyu Xie <96410222+ValerieTse@users.noreply.github.com>
2025-04-14 13:52:58 -0700

    Create sexcheck_chrY.r

---
+++ sexcheck_chrY.r
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+# ----- Load Required Library -----
+library(dplyr)
+
+# ----- Parse Command Line Arguments -----
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) < 3) {
+  stop("Usage: Rscript sexcheck_chrY.r <smiss_file> <sex_info> <prefix>")
+}
+
+smiss_file <- args[1]
+sex_info <- args[2]
+prefix <- args[3]
+
+# ----- Read and Process .smiss File -----
+smiss <- read.table(smiss_file, header = FALSE)
+colnames(smiss) <- c("FID", "IID", "PHENO1", "MISSING_CT", "OBS_CT", "F_MISS")
+smiss <- smiss %>% select(IID, F_MISS)
+
+sex_info <- read.table(sex_info, header = FALSE)
+colnames(sex_info) <- c("FID", "IID", "PEDSEX", "SNPSEX", "STATUS", "F")
+sex_info <- sex_info %>% select(IID, PEDSEX, SNPSEX, F)
+
+df <- merge(smiss, sex_info , by = "IID", all = TRUE) %>%
+      mutate(STATUS = ifelse((PEDSEX == 2 & is.na(F_MISS) == FALSE), 'PROBLEM','NORMAL'))
+
+write.table(df, file = paste0("DATA/", prefix,"_sexcheck_Ymiss.txt"),
+            quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+
+
